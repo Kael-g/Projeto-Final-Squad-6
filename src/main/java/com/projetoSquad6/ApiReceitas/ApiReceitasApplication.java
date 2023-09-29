386fbbd835c1654283d6dd678670ee9b9e7e6a7f
IrisRPerrorni <iris.perroni@zup.com.br>
2023-09-29 14:21:08 -0300

    :sparkles: feat:  subindo projeto

---
+++ src/main/java/com/projetoSquad6/ApiReceitas/ApiReceitasApplication.java
@@ -0,0 +1,13 @@
+package com.projetoSquad6.ApiReceitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiReceitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiReceitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoSquad6/ApiReceitas/ApiReceitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoSquad6.ApiReceitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReceitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
